609f3d1e7d243c5016e763d8f7d1febb0e09252d
don grigory <dingrygory29@gmail.com>
2022-10-13 08:44:58 +0300

    Initial Commit

---
+++ softApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  softApp
+//
+//  Created by Grigory Don on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
